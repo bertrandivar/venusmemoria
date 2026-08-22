d48625a0a2b00231e1c59073c9929ccf073f140c
bertrandivar <ngbguybertrand@gmail.com>
2026-08-22 11:36:17 +0200

    Add artCulinaireProducts with culinary tools

---
+++ src/data/products/artCulinaire.ts
@@ -0,0 +1,9 @@
+import { Product } from "../shopData";
+import { icingPipingBagProduct } from "./icingPipingBag";
+import { nozzleIcePipingProduct } from "./nozzleIcePiping";
+
+export const artCulinaireProducts: Product[] = [
+  icingPipingBagProduct,
+  nozzleIcePipingProduct,
+  // Vous pourrez ajouter ici plus tard d'autres outils culinaires (moules, pinceaux alimentaires, etc.)
+];
