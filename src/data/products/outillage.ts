c44fc1f1142ba7e69415220a28ece4b714b48115
bertrandivar <ngbguybertrand@gmail.com>
2026-08-22 10:59:35 +0200

    Add outillageProducts array with initial products

---
+++ src/data/products/outillage.ts
@@ -0,0 +1,9 @@
+import { Product } from "../shopData";
+import { agrafeuseNoireProduct } from "./agrafeuseNoire";
+import { agrafeuseJauneProduct } from "./agrafeuseJaune";
+
+export const outillageProducts: Product[] = [
+  agrafeuseNoireProduct,
+  agrafeuseJauneProduct,
+  // Vous pourrez ajouter ici plus tard : petitsClousProduct, agraphesSimplesProduct, etc.
+];
