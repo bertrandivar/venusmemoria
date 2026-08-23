100b1c2ef6411779de888c24301cacbedc7fbd22
bertrandivar <ngbguybertrand@gmail.com>
2026-08-23 13:46:31 +0200

    Update outillage.ts

--- src/data/products/outillage.ts
+++ src/data/products/outillage.ts
@@ -2,10 +2,11 @@ import { Product } from "../shopData";
 import { agrafeuseNoireProduct } from "./agrafeuseNoire";
 import { agrafeuseJauneProduct } from "./agrafeuseJaune";
 import { pistoletColleYellow } from "./pistoletColleYellow";
+import { batonsColleProduct } from "./batonsColle"; // <-- Import des bâtons de colle
 
 export const outillageProducts: Product[] = [
   agrafeuseNoireProduct,
   agrafeuseJauneProduct,
   pistoletColleYellow,
-  // Vous pourrez ajouter ici plus tard : petitsClousProduct, agraphesSimplesProduct, etc.
+  batonsColleProduct, // <-- Ajout à la liste d'outillage
 ];
