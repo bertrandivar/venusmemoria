466f2c543f3dfa2c0da70b34e7e4b395ffe5d5c8
bertrandivar <ngbguybertrand@gmail.com>
2026-08-23 10:49:17 +0200

    Update outillage.ts

--- src/data/products/outillage.ts
+++ src/data/products/outillage.ts
@@ -1,9 +1,11 @@
 import { Product } from "../shopData";
 import { agrafeuseNoireProduct } from "./agrafeuseNoire";
 import { agrafeuseJauneProduct } from "./agrafeuseJaune";
+import { pistoletColleYellow } from "./pistoletColleYellow";
 
 export const outillageProducts: Product[] = [
   agrafeuseNoireProduct,
   agrafeuseJauneProduct,
+  pistoletColleYellow,
   // Vous pourrez ajouter ici plus tard : petitsClousProduct, agraphesSimplesProduct, etc.
 ];
