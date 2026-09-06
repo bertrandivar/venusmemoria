342b39c5d39665924f7c8c1e0c4e57c740205614
bertrandivar <ngbguybertrand@gmail.com>
2026-09-06 12:32:42 +0200

    Rename handmadeBagsProducts to HANDMADE_BAGS_PRODUCTS

--- src/data/products/handmadeBags.ts
+++ src/data/products/handmadeBags.ts
@@ -1,7 +1,7 @@
 import { Product } from "../shopData";
 import { sacPerleMassaiProduct } from "./bags/sacPerleMassai";
 
-export const handmadeBagsProducts: Product[] = [
+export const HANDMADE_BAGS_PRODUCTS: Product[] = [
   sacPerleMassaiProduct,
   // Vous ajouterez vos futurs sacs ici facilement !
 ];
