bad723310e74a444ccea6f79110a03046c649959
bertrandivar <ngbguybertrand@gmail.com>
2026-09-06 11:58:56 +0200

    Add sandaleKitengeSafari product data

---
+++ src/data/products/sandals/sandaleKitengeSafari.ts
@@ -0,0 +1,21 @@
+import { Product } from "../../shopData";
+
+export const sandaleKitengeSafariProduct: Product = {
+  id: "sandale-kitenge-safari",
+  name: "Sandale Kitenge Safari",
+  category: "sandals",
+  price: 30000,
+  currency: "BIF",
+  description: "Sandales légères mariant le cuir souple et le tissu Kitenge coloré pour un style estival affirmé.",
+  badge: "Artisanat",
+  stockQuantity: 3,
+  images: [
+    "https://via.placeholder.com/600?text=Sandale+Kitenge+1",
+  ],
+  details: [
+    "Finitions en tissu Kitenge authentique",
+    "Semelle confortable et antidérapante",
+    "Design coloré et unique",
+  ],
+  specs: "Cuir & Tissu Kitenge",
+};
