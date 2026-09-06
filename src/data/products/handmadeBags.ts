e2016787c886c6ed0dd2ec5e8918fa671c43abf6
bertrandivar <ngbguybertrand@gmail.com>
2026-09-06 11:47:18 +0200

    Create handmadeBags.ts with product data
    Added a new product entry for handmade bags with details.

---
+++ src/data/products/handmadeBags.ts
@@ -0,0 +1,23 @@
+import { Product } from "../shopData";
+
+export const HANDMADE_BAGS_PRODUCTS: Product[] = [
+  {
+    id: "bag-beaded-01",
+    name: "Sac Perlé Artisanal Massai",
+    category: "mode",
+    price: 55000,
+    currency: "BIF",
+    images: [
+      "/assets/bags/beaded-bag-1.jpg"
+    ],
+    description: "Sac à main entièrement tissé à la main avec des perles artisanales de haute qualité.",
+    details: [
+      "100% fait main par des artisanes",
+      "Fermeture sécurisée",
+      "Bandoulière résistante"
+    ],
+    specs: "Perles synthétiques & Doublure tissu",
+    stockQuantity: 2,
+    badge: "Fait Main"
+  }
+];
